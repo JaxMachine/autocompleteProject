42de19bf7a13d9c7e263ef4fe665454688695278
JaxMachine <newtradesofjack@gmail.com>
2024-01-18 14:33:03 -0600

    Spine of The project is done

---
+++ script-test.js
@@ -0,0 +1,18 @@
+describe("Script Testing for Autocomplete Project", function(){
+
+    //Set Up input variables for testing
+    beforeEach(function(){
+        input.value = '';
+    });
+
+
+    it('should accept an input value and clear after search', function(){
+       
+    });
+
+
+    //Clean Up After Testing
+    afterEach(function(){
+        input.value ='';
+    });
+});
--- script.js
+++ script.js
@@ -4,31 +4,47 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+	let results = fruit.filter((f) => {
+		return f.toLowerCase().includes(str.toLowerCase());
+	});
+	showSuggestions(results, str);
 }
 
-function searchHandler(e) {
-	if(e.key === "Enter"){
-		console.log(input.value)
-		input.value='';
+function searchHandler(event) {
+	search(input.value);
+
+	if(event.key ==="Enter"){
+		console.log(input.value);
+		clearInput();
 	}
-
-
-
 }
 
 function showSuggestions(results, inputVal) {
 
-	// TODO
-}
+	const listed = results.map((result) =>{
+		if( inputVal !== ''){
+			return "<li>" + result + "</li>";
+		}
+	});
+	suggestions.innerHTML = "<ul>" + listed.join('')+ "</ul>";
+};
 
 function useSuggestion(e) {
-	// TODO
+	//intakes the suggested value
+	input.value = e.target.innerText;
+	console.log(input.value);
+	clearInput();
 }
 
+function clearInput(){
+	//Clear Input box
+	input.value = '';
+
+	//Clear Suggestion HTML
+	suggestions.innerHTML = '';
+}
+
+
+
 input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+suggestions.addEventListener('mouseup', useSuggestion);
